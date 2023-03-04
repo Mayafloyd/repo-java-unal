5482155ed7a69768262cc26861218b8b3170affb
Sara Maya <amii.lee.xd@gmail.com>
2023-03-04 08:00:56 -0500

    activity 1

---
+++ src/NrFour.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NrFour {
+    public static void main(String[] args) {
+        Integer mama,juan,alberto,ana;
+
+        System.out.println("Ingresa la edad de Juan");
+        Scanner scanner = new Scanner(System.in);
+        juan = scanner.nextInt();
+
+        alberto = 2 * (juan/3);
+        ana= 4*(juan/3);
+        mama= juan+alberto+ana;
+
+        System.out.println("Edad de Juan " + juan);
+        System.out.println("Edad de Alberto " + alberto);
+        System.out.println("Edad de Ana " + ana);
+        System.out.println("Edad de mamá "+ mama);
+    }
+}
