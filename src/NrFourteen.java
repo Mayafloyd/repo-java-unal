d1f3a8ae197abb2633abe0ed90776bbf6ce8a82b
Sara Maya <amii.lee.xd@gmail.com>
2023-03-04 09:44:17 -0500

    activity seventeen

--- src/NrFourteen.java
+++ src/NrFourteen.java
@@ -11,3 +11,4 @@ public class NrFourteen {
         System.out.println("El cubo de " + num + " es " + cubo);
     }
 }
+
---
+++ src/NrSeventeen.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NrSeventeen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el radio del circulo ");
+        double radio = scanner.nextDouble();
+        double areaC = Math.PI *  Math.pow(radio,2);
+        double longitudC = 2 * Math.PI * radio;
+        System.out.println("El área del círculo es "+ areaC);
+        System.out.println("La longitud de la circunferencia es " + longitudC);
+    }
+}
