f5bdd9b81e74f38c588adfec14fcde631c963a15
Sara Maya <amii.lee.xd@gmail.com>
2023-03-04 09:25:55 -0500

    activity fourteen

---
+++ src/NrFourteen.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NrFourteen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un número");
+        double num = scanner.nextInt();
+        double cuadrado = Math.pow(num,2);
+        double cubo = Math.pow(num,3);
+        System.out.println("El cuadrado de " + num + " es " + cuadrado);
+        System.out.println("El cubo de " + num + " es " + cubo);
+    }
+}
