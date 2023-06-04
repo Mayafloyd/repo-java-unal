2e95dcd8f40201959eaeeb7c8baaa71de88391e4
Sara Maya <amii.lee.xd@gmail.com>
2023-06-03 23:51:32 -0500

    punto 5 again

---
+++ src/Actividad4_2/Punto5/Persona.java
@@ -0,0 +1,17 @@
+package Actividad4_2.Punto5;
+
+public class Persona {
+    String nombre;
+    String apellidos;
+    String teléfono;
+    String dirección;
+
+    public Persona(String nombre, String apellidos, String teléfono,
+                   String dirección) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.teléfono = teléfono;
+        this.dirección = dirección;
+    }
+}
+
---
+++ src/Actividad4_2/Punto5/Principal.java
@@ -0,0 +1,10 @@
+package Actividad4_2.Punto5;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
