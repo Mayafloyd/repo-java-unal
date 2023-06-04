b63250604eb9cb8b94269bddd7ca190b3cebf667
Sara Maya <amii.lee.xd@gmail.com>
2023-06-03 19:37:27 -0500

    actividad 4 - ejercicio 3

---
+++ src/ClaseAbstracta/Fraccion.java
@@ -0,0 +1,97 @@
+package ClaseAbstracta;
+
+public class Fraccion extends Numerica {
+    private int numerador;
+    private int denominador;
+
+    public Fraccion(int numerador, int denominador) {
+        this.numerador = numerador;
+        if (denominador != 0) {
+            this.denominador = denominador;
+        } else {
+            throw new IllegalArgumentException("El denominador no puede ser cero.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numerador + "/" + denominador;
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (this == ob) {
+            return true;
+        }
+        if (ob == null || getClass() != ob.getClass()) {
+            return false;
+        }
+        Fraccion otra = (Fraccion) ob;
+        return numerador == otra.numerador && denominador == otra.denominador;
+    }
+
+    @Override
+    public Numerica sumar(Numerica numero) {
+        if (numero instanceof Fraccion) {
+            Fraccion otra = (Fraccion) numero;
+            int nuevoNumerador = (this.numerador * otra.denominador) + (otra.numerador * this.denominador);
+            int nuevoDenominador = this.denominador * otra.denominador;
+            return simplificarFraccion(new Fraccion(nuevoNumerador, nuevoDenominador));
+        } else {
+            throw new IllegalArgumentException("La suma solo es compatible con fracciones.");
+        }
+    }
+
+    @Override
+    public Numerica restar(Numerica numero) {
+        if (numero instanceof Fraccion) {
+            Fraccion otra = (Fraccion) numero;
+            int nuevoNumerador = (this.numerador * otra.denominador) - (otra.numerador * this.denominador);
+            int nuevoDenominador = this.denominador * otra.denominador;
+            return simplificarFraccion(new Fraccion(nuevoNumerador, nuevoDenominador));
+        } else {
+            throw new IllegalArgumentException("La resta solo es compatible con fracciones.");
+        }
+    }
+
+    @Override
+    public Numerica multiplicar(Numerica numero) {
+        if (numero instanceof Fraccion) {
+            Fraccion otra = (Fraccion) numero;
+            int nuevoNumerador = this.numerador * otra.numerador;
+            int nuevoDenominador = this.denominador * otra.denominador;
+            return simplificarFraccion(new Fraccion(nuevoNumerador, nuevoDenominador));
+        } else {
+            throw new IllegalArgumentException("La multiplicacion solo es compatible con fracciones.");
+        }
+    }
+
+    @Override
+    public Numerica dividir(Numerica numero) {
+        if (numero instanceof Fraccion) {
+            Fraccion otra = (Fraccion) numero;
+            int nuevoNumerador = this.numerador * otra.denominador;
+            int nuevoDenominador = this.denominador * otra.numerador;
+            if (nuevoDenominador == 0) {
+                throw new ArithmeticException("No se puede dividir por cero.");
+            }
+            return simplificarFraccion(new Fraccion(nuevoNumerador, nuevoDenominador));
+        } else {
+            throw new IllegalArgumentException("La division solo es compatible con fracciones.");
+        }
+    }
+
+    // Metodo auxiliar para simplificar una fraccion
+    private Fraccion simplificarFraccion(Fraccion fraccion) {
+        int gcd = gcd(fraccion.numerador, fraccion.denominador);
+        return new Fraccion(fraccion.numerador / gcd, fraccion.denominador / gcd);
+    }
+
+    // Metodo auxiliar para calcular el maximo comun divisor (GCD)
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
---
+++ src/ClaseAbstracta/PruebaFraccion.java
@@ -0,0 +1,35 @@
+package ClaseAbstracta;
+
+public class PruebaFraccion {
+    public static void main(String[] args) {
+        Fraccion fraccion1 = new Fraccion(1, 2);
+        Fraccion fraccion2 = new Fraccion(3, 4);
+
+        System.out.println("Fraccion 1: " + fraccion1.toString());
+        System.out.println("Fraccion 2: " + fraccion2.toString());
+
+        // Suma
+        Numerica suma = fraccion1.sumar(fraccion2);
+        System.out.println("Suma: " + suma.toString());
+
+        // Resta
+        Numerica resta = fraccion1.restar(fraccion2);
+        System.out.println("Resta: " + resta.toString());
+
+        // Multiplicación
+        Numerica multiplicacion = fraccion1.multiplicar(fraccion2);
+        System.out.println("Multiplicacion: " + multiplicacion.toString());
+
+        // Division
+        Numerica division = fraccion1.dividir(fraccion2);
+        System.out.println("Division: " + division.toString());
+
+        // Comprobación de igualdad
+        System.out.println("¿Fraccion 1 es igual a Fraccion 2? " + fraccion1.equals(fraccion2));
+
+        // Comprobación de igualdad con una nueva instancia de fracción con los mismos valores
+        Fraccion fraccion3 = new Fraccion(1, 2);
+        System.out.println("¿Fraccion 1 es igual a Fraccion 3? " + fraccion1.equals(fraccion3));
+    }
+}
+
